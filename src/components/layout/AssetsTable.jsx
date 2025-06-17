25c1ee38e59d46eb37627702be5ac43351f60f9e
Natalia Davydova <dav.nata@mail.ru>
2025-06-17 21:53:40 +0500

    added sorting table to the project

--- src/components/layout/AssetsTable.jsx
+++ src/components/layout/AssetsTable.jsx
@@ -1,3 +1,45 @@
+import { Table } from 'antd';
+import {useCrypto} from '../../context/crypto-context';
+
+const columns = [
+  {
+    title: 'Name',
+    dataIndex: 'name',
+    showSorterTooltip: { target: 'full-header' },
+    sorter: (a, b) => a.name.length - b.name.length,
+    sortDirection: ['descend']
+
+  },
+  {
+    title: 'Price, $',
+    dataIndex: 'price',
+    defaultSortOrder: 'descend',
+    sorter: (a, b) => a.price - b.price,
+  },
+  {
+    title: 'Amount',
+    dataIndex: 'amount',
+    defaultSortOrder: 'descend',
+    sorter: (a, b) => a.amount - b.amount,
+  },
+];
+
 export default function AssetsTable() {
-  return <div>11</div>
+
+  const {assets} = useCrypto();
+
+    const data = assets.map((item) => ({
+      key: item.id,
+      name: item.name,
+      price: item.price,
+      amount: item.amount
+    }))
+  return (
+    <Table
+    pagination={false}
+    columns={columns}
+    dataSource={data}
+    showSorterTooltip={{ target: 'sorter-icon' }}
+  />
+  )
 }
